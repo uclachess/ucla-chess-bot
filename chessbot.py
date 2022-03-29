34e1a24239946b7065d7eebb611afc0fb393c0ec
map0te <mapote@mapote.com>
2022-03-29 01:31:58 -0700

    added create command, works for engine games

--- chessbot.py
+++ chessbot.py
@@ -7,13 +7,16 @@ from cairosvg import svg2png
 from stockfish import Stockfish
 
 class Game:
-    def __init__(self, game_info=None):
-        self.game_type = game_info
-        self.engine = Stockfish(depth=20, parameters={"Skill Level": 20})
+    def __init__(self, interval, engine_playsfor, difficulty, game_type):
+        self.game_type = game_type
+        self.engine = Stockfish(depth=25, parameters={"Skill Level": difficulty, "Threads": 8})
         self.board = chess.Board() 
         self.votes = dict()
         self.moves = dict()
 
+        self.interval = interval;
+        self.engine_playsfor = engine_playsfor;
+        
     def engine_move(self):
         # make engine move
         self.engine.set_fen_position(self.board.fen())
@@ -48,7 +51,10 @@ class Game:
         return moves
 
     def generate_discord_board_image(self):
-        svg2png(bytestring=chess.svg.board(self.board),write_to='output.png')
+        if self.game_type == "engine" and self.engine_playsfor == chess.WHITE:
+            svg2png(bytestring=chess.svg.board(self.board, orientation=chess.BlACK),write_to='output.png')
+        else:
+            svg2png(bytestring=chess.svg.board(self.board),write_to='output.png')
         with open('output.png', 'rb') as f:
             return discord.File(f)
 
@@ -58,32 +64,54 @@ with open("./token", "r") as f:
 
 GUILD = "Lambda's bot testing shithole"
 
-game = Game()
+game = None
 bot = commands.Bot(command_prefix='$')
 
+channel = None
+
 @bot.command(name='list')
-async def _create(ctx):
+async def _list(ctx):
     moves = game.get_move_votes()
     msg = ""
     for move in moves:
         msg = msg+game.board.san(move)+": "+str(moves[move])+"\n"
     await ctx.channel.send(msg)
         
+@bot.command(name='create')
+async def _create(ctx, interval, engine_playsfor, difficulty, game_type):
+    try:
+        submit.start()
+        submit.change_interval(seconds=int(interval))
+        global game
+        if engine_playsfor == "white": 
+            game = Game(interval, chess.WHITE, difficulty, game_type)
+        else:
+            game = Game(interval, chess.BLACK, difficulty, game_type)
+        if game_type == "engine" and engine_playsfor == "white":
+            game.engine_move()
+    except:
+        return
+    await channel.send(file=game.generate_discord_board_image())
+
+@bot.command(name='stop')
+async def _stop(ctx):
+    try:
+        submit.stop()
+    except:
+        return
+    
+@tasks.loop(seconds=30)
+async def submit():
+    if(game.user_move()):
+        game.engine_move()
+        await channel.send(file=game.generate_discord_board_image())
 
 @bot.command(name='vote')
 async def _vote(ctx, move):
     if(game.submit_vote_move(ctx.author, move)):
         await ctx.message.add_reaction('\N{Thumbs Up Sign}')
     else:
-        await ctx.message.add_reaction('\N{Thumbs Down Sign}')
-
-@tasks.loop(minutes=.25)
-async def submit():
-    channel = discord.utils.get(discord.utils.find(lambda g: g.name == GUILD, bot.guilds).channels, name="general")
-    if(game.user_move()):
-        game.engine_move()
-        await channel.send(file=game.generate_discord_board_image())
-    
+        await ctx.message.add_reaction('\N{Thumbs Down Sign}')    
     
 @bot.event
 async def on_ready():
@@ -92,6 +120,7 @@ async def on_ready():
         f'{bot.user} has connected to:\n'
         f'{guild.name}(id: {guild.id})'
         )
-    submit.start()
+    global channel
+    channel = discord.utils.get(discord.utils.find(lambda g: g.name == GUILD, bot.guilds).channels, name="general")
 
 bot.run(TOKEN)
